f80ad351394a5aeaa9c02727b95b0df911627311
Yoon <nam24329266@gmail.com>
2021-11-05 01:10:33 +0900

    211105 // webView 띄우기

--- app/src/main/java/com/yoon/busappkt/MainActivity.kt
+++ app/src/main/java/com/yoon/busappkt/MainActivity.kt
@@ -1,11 +1,72 @@
 package com.yoon.busappkt
 
+import android.app.Dialog
+import android.graphics.Bitmap
+import android.net.http.SslError
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.view.View
+import android.webkit.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        webView.apply {
+            webViewClient = WebViewClientClass()
+
+            webChromeClient = object : WebChromeClient() {
+                override fun onCreateWindow(view: WebView?, isDialog: Boolean, isUserGesture: Boolean, resultMsg: Message?): Boolean {
+                    val newWebView = WebView(this@MainActivity).apply {
+                        webViewClient = WebViewClient()
+                        webChromeClient = WebChromeClient()
+                        settings.javaScriptEnabled = true
+                    }
+                    return super.onCreateWindow(view, isDialog, isUserGesture, resultMsg)
+
+                    newWebView.webChromeClient = object : WebChromeClient(){
+                        override fun onCloseWindow(window: WebView?) {
+
+                        }
+                    }
+
+                    (resultMsg?.obj as WebView.WebViewTransport).webView = newWebView
+                    resultMsg.sendToTarget()
+                    return true
+                }
+            }
+
+            settings.javaScriptEnabled = true
+            settings.setSupportMultipleWindows(true)
+            settings.javaScriptCanOpenWindowsAutomatically = true
+            settings.loadWithOverviewMode = true
+            settings.useWideViewPort =true
+            settings.setSupportZoom(false)
+        }
+
+        webView.loadUrl("https://www.aladin.co.kr")
+    }
+
+    inner class WebViewClientClass: WebViewClient(){
+        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            view.loadUrl(url)
+            return true;
+        }
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+            webView.visibility = View.INVISIBLE
+        }
+
+        override fun onPageCommitVisible(view: WebView?, url: String?) {
+            super.onPageCommitVisible(view, url)
+            webView.visibility = View.VISIBLE
+        }
     }
 }
